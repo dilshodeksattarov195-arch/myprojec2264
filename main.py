1a42ff811b2c123609e5aa88a2146a68bab10735
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-31 08:53:31 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class cacheDtringifyWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = cacheDtringifyWorker()
+    print("Worker engine initialized.")
